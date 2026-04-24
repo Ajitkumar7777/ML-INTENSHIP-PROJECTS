70571d18dafb59328c752744b786243dcfc2e088
Ajitkumar7777 <mr.ajitkumar1077@gmail.com>
2026-04-24 14:03:24 +0530

    Update movie-recommendation-system.ml

--- movie-recommendation-system.ml
+++ movie-recommendation-system.ml
@@ -1,16 +1,1 @@
-# Movie Recommendation System
 
-## 📌 Description
-This project recommends movies based on user ratings using Machine Learning.
-
-## ⚙️ Technologies Used
-- Python
-- Pandas
-- Scikit-learn
-
-## 📊 Method
-- Collaborative Filtering
-- Cosine Similarity
-
-## 📌 Output
-Top 5 recommended movies
